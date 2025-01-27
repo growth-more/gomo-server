645a3fa3c017c7cb5df0bf8a02cbf7790dbeb89e
jhl221123 <jhl221123@naver.com>
2025-01-28 00:13:40 +0900

    feat: 이미지 크기 초과 예외 처리

--- src/main/java/com/gomo/app/common/exception/DomainExceptionAdvice.java
+++ src/main/java/com/gomo/app/common/exception/DomainExceptionAdvice.java
@@ -1,10 +1,13 @@
 package com.gomo.app.common.exception;
 
+import static com.gomo.app.common.exception.DomainErrorCode.*;
+
 import java.time.LocalDateTime;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -25,5 +28,19 @@ public class DomainExceptionAdvice {
 		);
 
 		return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
+	}
+
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<ErrorResponse> handleBusinessException(MaxUploadSizeExceededException e, HttpServletRequest request) {
+		log.warn("[File upload exception occurred!] code: {}, message: {}", IMAGE_TOO_LARGE, e.getMessage(), e);
+		ErrorResponse errorResponse = ErrorResponse.of(
+			LocalDateTime.now(),
+			IMAGE_TOO_LARGE.getHttpStatus(),
+			IMAGE_TOO_LARGE.name(),
+			e.getMessage(),
+			request.getRequestURI()
+		);
+
+		return ResponseEntity.status(IMAGE_TOO_LARGE.getHttpStatus()).body(errorResponse);
 	}
 }
