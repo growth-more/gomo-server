5417fb37054c8c0187989a8fc067036a1b1fe055
jhl221123 <jhl221123@naver.com>
2025-02-03 23:09:11 +0900

    test: 순서 변경 서비스 테스트 수정

--- src/test/java/com/gomo/app/common/unit/OrderChangerTest.java
+++ src/test/java/com/gomo/app/common/unit/OrderChangerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import com.gomo.app.common.domain.service.DisplayOrder;
 import com.gomo.app.common.domain.service.OrderChangeable;
 import com.gomo.app.common.domain.service.OrderChanger;
+import com.gomo.app.common.exception.PolicyViolationException;
 
 import lombok.Getter;
 
@@ -27,6 +28,18 @@ public class OrderChangerTest {
 		assertThat(candidates)
 			.extracting(candidate -> ((MockOrderChangeable)candidate).getDisplayOrder())
 			.containsExactly(DisplayOrder.of(3), DisplayOrder.of(2), DisplayOrder.of(1));
+	}
+
+	@DisplayName("정렬 대상과 변경된 순서의 크기가 다르면 변경할 수 없다.")
+	@Test
+	void change_display_order_not_same_size() {
+		List<OrderChangeable> candidates = List.of(MockOrderChangeable.of(1), MockOrderChangeable.of(2));
+		List<DisplayOrder> changedOrders = List.of(DisplayOrder.of(3), DisplayOrder.of(2), DisplayOrder.of(1));
+
+		assertThatThrownBy(() -> OrderChanger.change(candidates, changedOrders))
+			.isInstanceOf(PolicyViolationException.class)
+			.hasMessageContaining("Candidates size and changed orders size must be the same");
+
 	}
 
 	@Getter
