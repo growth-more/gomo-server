9622bba00ec132c23216541eb827c0715c86be88
jhl221123 <jhl221123@naver.com>
2025-02-10 21:37:04 +0900

    test: 참여 퀘스트 생성 기능 테스트

---
+++ src/test/java/com/gomo/app/member/integration/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.gomo.app.member.integration;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gomo.app.common.IntegrationTestBase;
+import com.gomo.app.member.domain.model.ActivateStatus;
+import com.gomo.app.member.domain.model.Member;
+import com.gomo.app.member.domain.repository.MemberRepository;
+
+@DisplayName("[Domain integration]: 회원 DB 접근 테스트")
+public class MemberRepositoryTest extends IntegrationTestBase {
+
+	@Autowired
+	MemberRepository sut;
+
+	@DisplayName("활성화되어 있고, 어제까지 로그인한 사용자 목록을 조회한다.")
+	@Test
+	void find_active_and_login_member() {
+		List<Member> members = sut.findByActivateStatusAndLastLoginDateTimeGreaterThanEqual(
+			ActivateStatus.ACTIVE,
+			LocalDate.now().minusDays(1).atStartOfDay()
+		);
+
+		assertThat(members.size()).isEqualTo(1);
+	}
+}
