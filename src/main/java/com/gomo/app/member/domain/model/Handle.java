3ea185de8ec9710bc4c28a13d6eba0e2088dd8b5
kkj1250 <kkj1250@gmail.com>
2025-02-12 00:46:45 +0900

    feat: domain model 검증함수 추가

--- src/main/java/com/gomo/app/member/domain/model/Handle.java
+++ src/main/java/com/gomo/app/member/domain/model/Handle.java
@@ -1,13 +1,22 @@
 package com.gomo.app.member.domain.model;
 
 import com.gomo.app.common.domain.ValueObject;
+import com.gomo.app.common.exception.PolicyViolationException;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+
+import java.util.regex.Pattern;
+
+import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
 
 @Getter
 @Embeddable
 @ValueObject
 public class Handle {
+
+    private static final int MIN_HANDLE_LENGTH = 3;
+    private static final int MAX_HANDLE_LENGTH = 30;
+    private static final Pattern HANDLE_PATTERN = Pattern.compile("^[a-zA-Z0-9._-]+$");
 
     private String handle;
 
@@ -15,6 +24,9 @@ public class Handle {
     }
 
     private Handle(String handle) {
+        ensureNotBlank(handle);
+        ensureValidHandleLength(handle);
+        ensureValidHandleRule(handle);
         this.handle = handle;
     }
 
@@ -23,18 +35,33 @@ public class Handle {
     }
 
     public Handle update(String handle) {
+        if(this.handle.equals(handle)){
+            throw new PolicyViolationException(INVALID_PARAMETER, "Handle is already same");
+        }
+
         return Handle.of(handle);
     }
 
-    private boolean isValidLength(
-        int min,
-        int max
-    ) {
-        return false;
+    private void ensureNotBlank(String handle){
+        if (handle == null || handle.isBlank()) {
+            throw new PolicyViolationException(INVALID_PARAMETER,"Handle must not be blank");
+        }
     }
 
-    private boolean doesContainProhibitCharacters() {
-        return false;
+    private void ensureValidHandleLength(String handle) {
+        if(handle.length() < MIN_HANDLE_LENGTH){
+            throw new PolicyViolationException(INVALID_PARAMETER, "Handle must be at least 3 characters");
+        }
+
+        if(handle.length() > MAX_HANDLE_LENGTH){
+            throw new PolicyViolationException(INVALID_PARAMETER, "Handle must not exceed 30 characters");
+        }
+    }
+
+    private void ensureValidHandleRule(String handle){
+        if(!HANDLE_PATTERN.matcher(handle).matches()){
+            throw new PolicyViolationException(INVALID_PARAMETER, "Handle must contain only alphabets, numbers, and special characters: . _ -");
+        }
     }
 
     @Override
