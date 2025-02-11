cefb2eb44a59fad98062fc874bb74d991a11d77f
kkj1250 <kkj1250@gmail.com>
2025-02-12 00:46:45 +0900

    feat: domain model 검증함수 추가

--- src/main/java/com/gomo/app/member/domain/model/Password.java
+++ src/main/java/com/gomo/app/member/domain/model/Password.java
@@ -1,13 +1,22 @@
 package com.gomo.app.member.domain.model;
 
 import com.gomo.app.common.domain.ValueObject;
+import com.gomo.app.common.exception.PolicyViolationException;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+
+import java.util.regex.Pattern;
+
+import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
 
 @Getter
 @Embeddable
 @ValueObject
 public class Password {
+
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    private static final int MAX_PASSWORD_LENGTH = 64;
+    private static final Pattern PASSWORD_RULE_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!])[A-Za-z\\d@#$%^&+=!]+$");
 
     private String password;
 
@@ -15,6 +24,9 @@ public class Password {
     }
 
     private Password(String password) {
+        ensureNotBlank(password);
+        ensureValidLength(password);
+        ensureValidPasswordRule(password);
         this.password = password;
     }
 
@@ -26,11 +38,25 @@ public class Password {
         return Password.of(password);
     }
 
-    private boolean isValidLength(int min, int max) {
-        return false;
+    private void ensureNotBlank(String password){
+        if(password == null || password.isBlank()){
+            throw new PolicyViolationException(INVALID_PARAMETER, "password must not be blank");
+        }
     }
 
-    private boolean doesContainProhibitCharacters() {
-        return false;
+    private void ensureValidLength(String password){
+        if (password.length() > MAX_PASSWORD_LENGTH) {
+            throw new PolicyViolationException(INVALID_PARAMETER, "password must not exceed 64 characters");
+        }
+
+        if (password.length() < MIN_PASSWORD_LENGTH){
+            throw new PolicyViolationException(INVALID_PARAMETER, "password must at least 8 characters");
+        }
+    }
+
+    private void ensureValidPasswordRule(String password){
+        if(!PASSWORD_RULE_PATTERN.matcher(password).matches()){
+            throw new PolicyViolationException(INVALID_PARAMETER, "password must not contain forbidden characters");
+        }
     }
 }
