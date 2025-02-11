d5ae0d2e8716913fd8381112af4282aa86273787
kkj1250 <kkj1250@gmail.com>
2025-02-12 00:46:45 +0900

    feat: domain model 검증함수 추가

--- src/main/java/com/gomo/app/member/domain/model/Password.java
+++ src/main/java/com/gomo/app/member/domain/model/Password.java
@@ -2,15 +2,12 @@ package com.gomo.app.member.domain.model;
 
 import com.gomo.app.common.domain.ValueObject;
 import com.gomo.app.common.exception.PolicyViolationException;
-import com.gomo.app.member.domain.service.PasswordService;
-import com.gomo.app.member.exception.MemberAuthenticationFailedException;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
 import java.util.regex.Pattern;
 
 import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
-import static com.gomo.app.member.exception.MemberErrorCode.AUTHENTICATION_FAILED;
 
 @Getter
 @Embeddable
@@ -27,33 +24,27 @@ public class Password {
     }
 
     private Password(String password) {
+        ensureNotBlank(password);
+        ensureValidLength(password);
+        ensureValidPasswordRule(password);
         this.password = password;
     }
 
-    public static Password fromRaw(String rawPassword) {
-        ensureNotBlank(rawPassword);
-        ensureValidLength(rawPassword);
-        ensureValidPasswordRule(rawPassword);
-        return new Password(rawPassword);
+    public static Password of(String password) {
+        return new Password(password);
     }
 
-    public static Password fromEncoded(String encodedPassword) {
-        return new Password(encodedPassword);
+    public Password update(String password) {
+        return Password.of(password);
     }
 
-    public void matches(String rawPassword, boolean isMatched){
-        if(!isMatched){
-            throw new MemberAuthenticationFailedException(AUTHENTICATION_FAILED, "password incorrect");
-        }
-    }
-
-    private static void ensureNotBlank(String password){
+    private void ensureNotBlank(String password){
         if(password == null || password.isBlank()){
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not be blank");
         }
     }
 
-    private static void ensureValidLength(String password){
+    private void ensureValidLength(String password){
         if (password.length() > MAX_PASSWORD_LENGTH) {
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not exceed 64 characters");
         }
@@ -63,7 +54,7 @@ public class Password {
         }
     }
 
-    private static void ensureValidPasswordRule(String password){
+    private void ensureValidPasswordRule(String password){
         if(!PASSWORD_RULE_PATTERN.matcher(password).matches()){
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not contain forbidden characters");
         }
