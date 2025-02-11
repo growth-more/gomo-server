689b1d3312cc3af092a7d1e6d97bc96a6e492333
kkj1250 <kkj1250@gmail.com>
2025-02-12 00:46:45 +0900

    feat: domain model 검증함수 추가

--- src/main/java/com/gomo/app/member/domain/model/MemberName.java
+++ src/main/java/com/gomo/app/member/domain/model/MemberName.java
@@ -2,13 +2,23 @@ package com.gomo.app.member.domain.model;
 
 import com.gomo.app.common.domain.ValueObject;
 
+import com.gomo.app.common.exception.DomainErrorCode;
+import com.gomo.app.common.exception.PolicyViolationException;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+
+import java.util.regex.Pattern;
+
+import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
 
 @Getter
 @Embeddable
 @ValueObject
 public class MemberName {
+
+    private static final int MAX_NAME_LENGTH = 20;
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final Pattern NAME_RULE = Pattern.compile("^(?!.*\\\\.\\\\.)(?!.*--)(?!.*__)[a-zA-Z0-9ㄱ-힣._-]+$");
 
     private String name;
 
@@ -16,6 +26,9 @@ public class MemberName {
     }
 
     private MemberName(String name) {
+        ensureNotBlank(name);
+        ensureNameLength(name);
+        ensureNameRule(name);
         this.name = name;
     }
 
@@ -24,18 +37,32 @@ public class MemberName {
     }
 
     public MemberName update(String name) {
+        if (this.name.equals(name)){
+            throw new PolicyViolationException(INVALID_PARAMETER, "name must not be same with origin name");
+        }
         return MemberName.of(name);
     }
 
-    private boolean isValidLength(
-        int min,
-        int max
-    ) {
-        return false;
+    private void ensureNotBlank(String name){
+        if(name == null || name.isBlank()){
+            throw new PolicyViolationException(INVALID_PARAMETER, "name must not be blank");
+        }
     }
 
-    private boolean doesContainProhibitCharacters() {
-        return false;
+    private void ensureNameLength(String name){
+        if(name.length() < MIN_NAME_LENGTH){
+            throw new PolicyViolationException(INVALID_PARAMETER, "name must not be less than 2 characters");
+        }
+
+        if(name.length() > MAX_NAME_LENGTH){
+            throw new PolicyViolationException(INVALID_PARAMETER, "name must not be more than 20 characters");
+        }
+    }
+
+    private void ensureNameRule(String name){
+        if(!NAME_RULE.matcher(name).matches()){
+            throw new PolicyViolationException(INVALID_PARAMETER, "name must contain only letters and numbers");
+        }
     }
 
     @Override
