ef7132d9a21bb97a7f3bc5732ecb8016b7d03deb
kkj1250 <kkj1250@gmail.com>
2025-02-14 06:13:11 +0900

    feat: JWT email 인증기능 구현

--- src/main/java/com/gomo/app/common/configuration/WebConfiguration.java
+++ src/main/java/com/gomo/app/common/configuration/WebConfiguration.java
@@ -14,6 +14,7 @@ import com.gomo.app.common.converter.OctetStreamReadMsgConverter;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final AuthArgumentResolver authArgumentResolver;
