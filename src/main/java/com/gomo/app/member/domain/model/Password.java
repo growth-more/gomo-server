86b4b2dfe0ce8c9aa1ec40c921bc0ab0b10c63f6
kkj1250 <kkj1250@gmail.com>
2025-02-14 06:10:13 +0900

    feat: 비밀번호 관련 기능 구현
    - BCrypt사용, 비밀번호 암호화
    - 비밀번호 변경 기능 구현

--- src/main/java/com/gomo/app/member/domain/model/Password.java
+++ src/main/java/com/gomo/app/member/domain/model/Password.java
@@ -2,12 +2,15 @@ package com.gomo.app.member.domain.model;
 
 import com.gomo.app.common.domain.ValueObject;
 import com.gomo.app.common.exception.PolicyViolationException;
+import com.gomo.app.member.domain.service.PasswordService;
+import com.gomo.app.member.exception.MemberAuthenticationFailedException;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
 import java.util.regex.Pattern;
 
 import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
+import static com.gomo.app.member.exception.MemberErrorCode.AUTHENTICATION_FAILED;
 
 @Getter
 @Embeddable
@@ -24,27 +27,33 @@ public class Password {
     }
 
     private Password(String password) {
-        ensureNotBlank(password);
-        ensureValidLength(password);
-        ensureValidPasswordRule(password);
         this.password = password;
     }
 
-    public static Password of(String password) {
-        return new Password(password);
+    public static Password fromRaw(String rawPassword) {
+        ensureNotBlank(rawPassword);
+        ensureValidLength(rawPassword);
+        ensureValidPasswordRule(rawPassword);
+        return new Password(rawPassword);
     }
 
-    public Password update(String password) {
-        return Password.of(password);
+    public static Password fromEncoded(String encodedPassword) {
+        return new Password(encodedPassword);
     }
 
-    private void ensureNotBlank(String password){
+    public void matches(String rawPassword, boolean isMatched){
+        if(!isMatched){
+            throw new MemberAuthenticationFailedException(AUTHENTICATION_FAILED, "password incorrect");
+        }
+    }
+
+    private static void ensureNotBlank(String password){
         if(password == null || password.isBlank()){
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not be blank");
         }
     }
 
-    private void ensureValidLength(String password){
+    private static void ensureValidLength(String password){
         if (password.length() > MAX_PASSWORD_LENGTH) {
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not exceed 64 characters");
         }
@@ -54,7 +63,7 @@ public class Password {
         }
     }
 
-    private void ensureValidPasswordRule(String password){
+    private static void ensureValidPasswordRule(String password){
         if(!PASSWORD_RULE_PATTERN.matcher(password).matches()){
             throw new PolicyViolationException(INVALID_PARAMETER, "password must not contain forbidden characters");
         }
--- src/main/java/com/gomo/app/member/presentation/PasswordMemberApi.java
+++ src/main/java/com/gomo/app/member/presentation/PasswordMemberApi.java
@@ -1,5 +1,7 @@
 package com.gomo.app.member.presentation;
 
+import com.gomo.app.common.authentication.Auth;
+import com.gomo.app.member.domain.model.MemberId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +21,8 @@ public class PasswordMemberApi {
 	private final UpdateMemberUseCase updateMemberUseCase;
 
 	@PutMapping
-	public ResponseEntity<Void> update(@RequestBody UpdatePasswordRequest request) {
-		return null;
+	public ResponseEntity<Void> update(@Auth MemberId memberId, @RequestBody UpdatePasswordRequest request) {
+		updateMemberUseCase.updatePassword(memberId, request);
+		return ResponseEntity.ok().build();
 	}
 }
