84adb171170cdc32a96f131337e1a031a05228e1
jhl221123 <jhl221123@naver.com>
2025-02-19 00:19:24 +0900

    docs: api 문서 최신화 및 기능 점검
    - 회원 기능 점검 및 문서 최신화
    - 인증 기능 점검 및 문서 최신화

--- buildSrc/src/main/java/com/gomo/app/common/util/CommonUtils.java
+++ buildSrc/src/main/java/com/gomo/app/common/util/CommonUtils.java
@@ -6,29 +6,29 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class CommonUtils {
-    private static final String COOKIE_AUTH = "cookieAuth";
+
+    private static final String BEARER_AUTH = "bearerAuth";
 
     public static String addAuthOption(String content) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonObject jsonObject = gson.fromJson(content, JsonObject.class);
 
         JsonObject securitySchemes = new JsonObject();
-        JsonObject cookieAuthObject = new JsonObject();
-        cookieAuthObject.addProperty("type", "apiKey");
-        cookieAuthObject.addProperty("in", "cookie");
-        cookieAuthObject.addProperty("name", "Access-Token");
-        securitySchemes.add(COOKIE_AUTH, cookieAuthObject);
+        JsonObject bearerAuthObject = new JsonObject();
+
+        bearerAuthObject.addProperty("type", "http");
+        bearerAuthObject.addProperty("scheme", "bearer");
+        bearerAuthObject.addProperty("bearerFormat", "JWT");
+        securitySchemes.add(BEARER_AUTH, bearerAuthObject);
 
         jsonObject.getAsJsonObject("components")
-                .add("securitySchemes", securitySchemes);
+            .add("securitySchemes", securitySchemes);
 
         jsonObject.getAsJsonObject("paths").entrySet().forEach(path -> {
-            System.out.println("path key: "+path.getKey());
-            System.out.println("path value: "+path.getValue());
             path.getValue().getAsJsonObject().entrySet().forEach(operation -> {
                 JsonArray securityRequirement = new JsonArray();
                 JsonObject securityObject = new JsonObject();
-                securityObject.add(COOKIE_AUTH, new JsonArray());
+                securityObject.add(BEARER_AUTH, new JsonArray());
                 securityRequirement.add(securityObject);
                 operation.getValue().getAsJsonObject().add("security", securityRequirement);
             });
@@ -36,7 +36,7 @@ public class CommonUtils {
 
         JsonArray globalSecurity = new JsonArray();
         JsonObject globalSecurityObject = new JsonObject();
-        globalSecurityObject.add(COOKIE_AUTH, new JsonArray());
+        globalSecurityObject.add(BEARER_AUTH, new JsonArray());
         globalSecurity.add(globalSecurityObject);
 
         jsonObject.add("security", globalSecurity);
