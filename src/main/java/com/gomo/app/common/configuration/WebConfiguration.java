aeca8f32ef735da3860d118ef1be84bb93f52e39
kkj1250 <kkj1250@gmail.com>
2025-02-19 02:59:43 +0900

    fix: update cors origins

--- src/main/java/com/gomo/app/common/configuration/WebConfiguration.java
+++ src/main/java/com/gomo/app/common/configuration/WebConfiguration.java
@@ -26,7 +26,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:63342")
+			.allowedOrigins("http://localhost:63342", "https://gomo.nurdykim.me/")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
