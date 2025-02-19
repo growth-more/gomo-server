d5401130c23063e2f3b72b2ca597666cbeeb0cdf
jhl221123 <118912510+jhl221123@users.noreply.github.com>
2025-02-19 21:02:47 +0900

    [GOMO-69] api 문서 최신화 및 기능 점검 (#17)
    docs: api 문서 최신화 및 기능 점검
     - 회원 기능 점검 및 문서 최신화
     - 인증 기능 점검 및 문서 최신화

--- src/test/java/com/gomo/app/member/documentation/snippet/CheckDuplicatedHandleSnippet.java
+++ src/test/java/com/gomo/app/member/documentation/snippet/CheckDuplicatedHandleSnippet.java
@@ -1,7 +1,9 @@
 package com.gomo.app.member.documentation.snippet;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 
+import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
@@ -14,13 +16,18 @@ public class CheckDuplicatedHandleSnippet {
 	private static final String DESCRIPTION = "핸들 중복 및 유효성을 체크하고 상태 코드를 반환합니다.";
 	private static final String TAG = "Member";
 
+	private static final ParameterDescriptor[] CHECK_DUPLICATED_HANDLE_QUERY_PARAMETERS = {
+		parameterWithName("handle").description("핸들")
+	};
+
 	public static RestDocumentationFilter create() {
 		return document(
 			IDENTIFIER,
 			ResourceSnippetParameters.builder()
 				.summary(SUMMARY)
 				.description(DESCRIPTION)
-				.tag(TAG)
+				.tag(TAG),
+			queryParameters(CHECK_DUPLICATED_HANDLE_QUERY_PARAMETERS)
 		);
 	}
 
@@ -31,7 +38,8 @@ public class CheckDuplicatedHandleSnippet {
 				.summary(SUMMARY)
 				.description(DESCRIPTION)
 				.tag(TAG),
-			ErrorResponseFields.RESPONSE_FIELDS
+			ErrorResponseFields.RESPONSE_FIELDS,
+			queryParameters(CHECK_DUPLICATED_HANDLE_QUERY_PARAMETERS)
 		);
 	}
 }
