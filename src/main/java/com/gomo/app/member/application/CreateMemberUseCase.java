de1b67b9d36459ccd109e4fee91692717aff294e
kkj1250 <kkj1250@gmail.com>
2025-02-19 16:17:42 +0900

    feat: Member생성 시, PointWallet 생성 추가

--- src/main/java/com/gomo/app/member/application/CreateMemberUseCase.java
+++ src/main/java/com/gomo/app/member/application/CreateMemberUseCase.java
@@ -8,6 +8,11 @@ import com.gomo.app.member.domain.repository.MemberRepository;
 import com.gomo.app.member.domain.service.PasswordService;
 import com.gomo.app.member.presentation.request.CreateMemberRequest;
 import com.gomo.app.member.presentation.response.CreateMemberResponse;
+import com.gomo.app.point.domain.model.PointWallet;
+import com.gomo.app.point.domain.model.PointWalletId;
+import com.gomo.app.point.domain.model.TransactorId;
+import com.gomo.app.point.domain.repository.PointWalletRepository;
+import com.gomo.app.point.domain.service.PointWalletService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -18,13 +23,20 @@ import java.util.UUID;
 public class CreateMemberUseCase {
 
     private final MemberRepository memberRepository;
+    private final PointWalletRepository pointWalletRepository;
     private final PasswordService passwordService;
 
     public CreateMemberResponse create(CreateMemberRequest request) {
-        UUID uuid = UUIDGenerator.generate();
-        MemberId memberId = MemberId.of(uuid);
+        UUID member_uuid = UUIDGenerator.generate();
+        MemberId memberId = MemberId.of(member_uuid);
         Member member = request.toDomain(memberId, passwordService);
         Member savedMember = memberRepository.save(member);
+
+        UUID pointwallet_uuid = UUIDGenerator.generate();
+        PointWalletId pointWalletId = PointWalletId.of(pointwallet_uuid);
+        PointWallet pointWallet = PointWallet.createDefault(pointWalletId, TransactorId.of(member.getId().getId()));
+
+
         return CreateMemberResponse.of(savedMember.getId());
     }
 }
