ba34af9fd4ffad19a19ec43ccd290ee072209ad9
kkj1250 <kkj1250@gmail.com>
2025-03-10 09:38:54 +0900

    test: logout 테스트 작성완료

--- src/test/java/com/gomo/app/member/documentation/LogoutMemberDocumentationTest.java
+++ src/test/java/com/gomo/app/member/documentation/LogoutMemberDocumentationTest.java
@@ -1,49 +1,66 @@
 package com.gomo.app.member.documentation;
 
-import static io.restassured.RestAssured.*;
-import static org.springframework.http.HttpHeaders.*;
-import static org.springframework.http.HttpStatus.*;
-import static org.springframework.http.MediaType.*;
-
+import com.gomo.app.common.DocumentationTestBase;
 import com.gomo.app.common.util.LoginMemberHelper;
+import com.gomo.app.member.common.util.MemberDBDataHelper;
 import com.gomo.app.member.documentation.snippet.LogoutMemberSnippet;
+import com.gomo.app.member.documentation.snippet.UpdateHandleSnippet;
+import com.gomo.app.member.presentation.request.UpdateHandleRequest;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
 
-import com.gomo.app.common.DocumentationTestBase;
+import static com.gomo.app.common.exception.DomainErrorCode.INVALID_PARAMETER;
+import static com.gomo.app.member.exception.MemberErrorCode.HANDLE_DUPLICATED;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@DisplayName("[Presentation documentation]: 회원 로그아웃 테스트")
+@DisplayName("[Presentation documentation]: 핸들 변경 테스트")
 public class LogoutMemberDocumentationTest extends DocumentationTestBase {
 
-    private static final String LOGOUT_URL = "/members/logout";
+	private static final String LOGOUT_MEMBER_URL = "/members/logout";
 
-    private final RestDocumentationFilter filter = LogoutMemberSnippet.create();
+	private final RestDocumentationFilter filter = LogoutMemberSnippet.create();
+	private final RestDocumentationFilter errorFilter = LogoutMemberSnippet.createError();
 
-    private static final String EMAIL = "gomotest@naver.com";
+	private static final String EMAIL = "gomotest@naver.com";
 	private static final String PASSWORD = "Gomotest1234@";
 
 	private String token;
 
 	@Autowired
-    LoginMemberHelper loginMemberHelper;
+	LoginMemberHelper loginMemberHelper;
+
+	@Autowired
+	MemberDBDataHelper memberDBDataHelper;
 
 	@BeforeEach
 	public void setUp() {
 		token = loginMemberHelper.getAccessToken(EMAIL, PASSWORD);
 	}
 
-//    @DisplayName("사용자가 로그아웃한다.")
-//    @Test
-//    void logout_member(){
-//        given(this.specification).filter(filter)
-//                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-//                .header(AUTHORIZATION, "Bearer " + token)
-//                .when()
-//                .get(LOGOUT_URL)
-//                .then()
-//                .statusCode(OK.value());
-//    }
+	@AfterEach
+	public void tearDown() {
+		memberDBDataHelper.cleanUp();
+	}
+
+	@DisplayName("사용자가 로그아웃을 시도한다.")
+	@Test
+	void test_logout_member() {
+		given(this.specification).filter(filter)
+			.header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+			.header(AUTHORIZATION, "Bearer " + token)
+			.when()
+			.get(LOGOUT_MEMBER_URL)
+			.then()
+			.statusCode(OK.value());
+	}
 }
