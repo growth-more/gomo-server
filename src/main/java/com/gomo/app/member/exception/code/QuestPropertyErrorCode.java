863701abbd24a374f3276073e3c9c65d1ee04d15
jhl221123 <jhl221123@naver.com>
2025-04-26 20:17:27 +0900

    fix: 에러 코드 수정

--- src/main/java/com/gomo/app/member/exception/code/QuestPropertyErrorCode.java
+++ src/main/java/com/gomo/app/member/exception/code/QuestPropertyErrorCode.java
@@ -7,7 +7,7 @@ public enum QuestPropertyErrorCode {
 
 	TOO_SMALL(422, "MEM-QUE-001", "Threshold must meet the minimum value"),
 	TOO_LARGE(422, "MEM-QUE-002", "Threshold must meet the maximum value"),
-	UNEXPECTED_QUEST_TYPE(422, "MEM-QUE-002", "Unexpected quest type");
+	UNEXPECTED_QUEST_TYPE(422, "MEM-QUE-003", "Unexpected quest type");
 
 	private final int httpStatus;
 	private final String errorCode;
