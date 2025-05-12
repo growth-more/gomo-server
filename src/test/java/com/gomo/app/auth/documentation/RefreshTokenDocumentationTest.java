5bd17ce20f9cfcd530521863520a9d0595ba3a10
nurdy-kim <kkj1250@gmail.com>
2025-05-13 00:23:14 +0900

    test: auth도메인 테스트 작성 및 보완 완료

---
+++ src/test/java/com/gomo/app/auth/documentation/RefreshTokenDocumentationTest.java
@@ -0,0 +1,34 @@
+package com.gomo.app.auth.documentation;
+
+import static io.restassured.RestAssured.*;
+import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.restassured.RestDocumentationFilter;
+
+import com.gomo.app.auth.documentation.snippet.RefreshTokenSnippet;
+import com.gomo.app.common.DocumentationTestBase;
+
+@DisplayName("[Presentation Documentation]: Refresh Token 재발급 테스트")
+public class RefreshTokenDocumentationTest extends DocumentationTestBase {
+
+	private static final String REFRESH_TOKEN_URL = "/members/refresh";
+
+	private final RestDocumentationFilter filter = RefreshTokenSnippet.create();
+	private final RestDocumentationFilter errorFilter = RefreshTokenSnippet.createError();
+
+	@DisplayName("Access Token 갱신에 성공한다.")
+	@Test
+	void refresh_access_token() {
+		given(this.specification).filter(filter)
+			.header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+			.cookie("refreshToken", refreshToken)
+			.when()
+			.post(REFRESH_TOKEN_URL)
+			.then()
+			.statusCode(OK.value());
+	}
+}
--- src/test/java/com/gomo/app/auth/documentation/snippet/LoginMemberSnippet.java
+++ src/test/java/com/gomo/app/auth/documentation/snippet/LoginMemberSnippet.java
@@ -1,4 +1,4 @@
-package com.gomo.app.member.documentation.snippet;
+package com.gomo.app.auth.documentation.snippet;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -11,22 +11,21 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.gomo.app.common.constant.ErrorResponseFields;
 
-public class CreateMemberSnippet {
-	private static final String IDENTIFIER = "create_member";
-	private static final String SUMMARY = "회원 생성 API";
-	private static final String DESCRIPTION = "회원을 생성합니다.";
+public class LoginMemberSnippet {
+
+	private static final String IDENTIFIER = "login_member";
+	private static final String SUMMARY = "로그인 API";
+	private static final String DESCRIPTION = "회원 로그인 기능을 테스트합니다.";
 	private static final String TAG = "Member";
 
 	private static final Snippet REQUEST_FIELDS = requestFields(
 		fieldWithPath("email").type(JsonFieldType.STRING).description("회원 이메일"),
-		fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
-		fieldWithPath("handle").type(JsonFieldType.STRING).description("사용자 고유식별자"),
-		fieldWithPath("name").type(JsonFieldType.STRING).description("사용자 이름"),
-		fieldWithPath("motto").type(JsonFieldType.STRING).description("사용자 이름")
+		fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
 	);
 
 	private static final Snippet RESPONSE_FIELDS = responseFields(
-		fieldWithPath("id").type(JsonFieldType.STRING).description("사용자 고유식별자 ID")
+		fieldWithPath("memberId").type(JsonFieldType.STRING).description("회원 식별자 ID"),
+		fieldWithPath("token").type(JsonFieldType.STRING).description("Access Token")
 	);
 
 	public static RestDocumentationFilter create() {
@@ -36,8 +35,8 @@ public class CreateMemberSnippet {
 				.summary(SUMMARY)
 				.description(DESCRIPTION)
 				.tag(TAG)
-				.requestSchema(Schema.schema("CreateMemberRequest"))
-				.responseSchema(Schema.schema("CreateMemberResponse")),
+				.requestSchema(Schema.schema("LoginMemberRequest"))
+				.responseSchema(Schema.schema("LoginMemberResponse")),
 			REQUEST_FIELDS,
 			RESPONSE_FIELDS
 		);
@@ -50,10 +49,11 @@ public class CreateMemberSnippet {
 				.summary(SUMMARY)
 				.description(DESCRIPTION)
 				.tag(TAG)
-				.requestSchema(Schema.schema("CreateMemberRequest"))
+				.requestSchema(Schema.schema("LoginMemberRequest"))
 				.responseSchema(Schema.schema("ErrorResponse")),
 			REQUEST_FIELDS,
 			ErrorResponseFields.RESPONSE_FIELDS
 		);
+
 	}
 }
