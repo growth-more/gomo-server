7b01c40cb30737fe7ea975dde726ae94bed21c94
nurdy-kim <kkj1250@gmail.com>
2025-06-21 20:33:44 +0900

    feat: 비밀번호 초기화 로직 구현(이메일 인증, 비밀번호 변경)

---
+++ src/main/java/com/gomo/app/member/application/ResetPasswordUseCase.java
@@ -0,0 +1,29 @@
+package com.gomo.app.member.application;
+
+import com.gomo.app.common.ApplicationService;
+import com.gomo.app.member.domain.model.Email;
+import com.gomo.app.member.domain.model.Member;
+import com.gomo.app.member.domain.model.Password;
+import com.gomo.app.member.domain.service.MemberService;
+import com.gomo.app.member.domain.service.PasswordService;
+import com.gomo.app.member.presentation.request.ResetPasswordRequest;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@ApplicationService
+@Transactional
+public class ResetPasswordUseCase {
+
+	private final MemberService memberService;
+	private final PasswordService passwordService;
+
+	public void reset(ResetPasswordRequest request) {
+		Member member = memberService.findByEmail(Email.of(request.getEmail()));
+
+		Password rawNew = Password.ofRaw(request.getUpdatedPassword());
+
+		member.resetPassword(passwordService, rawNew);
+	}
+}
