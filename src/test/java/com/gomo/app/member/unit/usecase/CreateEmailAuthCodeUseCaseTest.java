e4b19825a7be8d314d9e4e227f9f820b1f81381f
nurdy-kim <kkj1250@gmail.com>
2025-06-21 20:52:09 +0900

    test: 비밀번호 초기화 관련 테스트 추가/수정

--- src/test/java/com/gomo/app/member/unit/usecase/CreateEmailAuthCodeUseCaseTest.java
+++ src/test/java/com/gomo/app/member/unit/usecase/CreateEmailAuthCodeUseCaseTest.java
@@ -17,6 +17,7 @@ import com.gomo.app.member.domain.service.AuthCodeGenerator;
 import com.gomo.app.member.domain.service.MemberService;
 import com.gomo.app.member.infrastructure.EmailAuthSenderService;
 import com.gomo.app.member.presentation.request.CreateEmailAuthCodeRequest;
+import com.gomo.app.member.presentation.request.CreatePasswordAuthCodeRequest;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("[Application unit] : 이메일 인증코드 생성 및 전송 테스트")
@@ -40,13 +41,23 @@ public class CreateEmailAuthCodeUseCaseTest {
 	private static final String EMAIL = "test@gmail.com";
 	private static final String AUTH_CODE = "123456";
 
-	@DisplayName("이메일 인증 코드를 생성한다.")
+	@DisplayName("회원가입 관련 이메일 인증 코드를 생성한다.")
 	@Test
 	void create_email_auth_code_successfully() {
 		doNothing().when(memberService).checkEmailDuplicated(any(Email.class));
 		doReturn(AUTH_CODE).when(authCodeGenerator).generate();
 		doNothing().when(emailAuthSenderService).sendEmailAuthCode(anyString(), anyString());
-		
+
 		assertThatCode(() -> sut.create(CreateEmailAuthCodeRequest.of(EMAIL))).doesNotThrowAnyException();
+	}
+
+	@DisplayName("비밀번호 초기화 관련 이메일 인증 코드를 생성한다.")
+	@Test
+	void create_email_auth_code_for_password_successfully() {
+		doReturn(null).when(memberService).findByEmail(any(Email.class));
+		doReturn(AUTH_CODE).when(authCodeGenerator).generate();
+		doNothing().when(emailAuthSenderService).sendEmailAuthCode(anyString(), anyString());
+
+		assertThatCode(() -> sut.createPwReset(CreatePasswordAuthCodeRequest.of(EMAIL))).doesNotThrowAnyException();
 	}
 }
