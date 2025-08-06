fec56eac501b5a927f44f37e22d468b38c55dd5f
Junhyeok Lee <jhl221123@naver.com>
2025-08-06 15:34:47 +0900

    [GOMO-185] 퀘스트 자동 생성 기능 비활성화 (#67)

--- src/main/java/com/gomo/app/quest/application/scheduler/CreateAssignQuestScheduler.java
+++ src/main/java/com/gomo/app/quest/application/scheduler/CreateAssignQuestScheduler.java
@@ -7,29 +7,21 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.NotNull;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.gomo.app.common.util.UUIDGenerator;
 import com.gomo.app.displayorder.DisplayOrder;
 import com.gomo.app.member.domain.model.ActivateStatus;
 import com.gomo.app.member.domain.repository.MemberRepository;
 import com.gomo.app.quest.application.translator.ParticipantTranslator;
 import com.gomo.app.quest.domain.model.AssignQuest;
-import com.gomo.app.quest.domain.model.AssignQuestId;
 import com.gomo.app.quest.domain.model.Participant;
 import com.gomo.app.quest.domain.model.ParticipantId;
-import com.gomo.app.quest.domain.model.Quest;
-import com.gomo.app.quest.domain.model.QuestContent;
 import com.gomo.app.quest.domain.model.QuestQuota;
 import com.gomo.app.quest.domain.model.QuestType;
 import com.gomo.app.quest.domain.model.RepeatQuest;
-import com.gomo.app.quest.domain.model.SubjectId;
-import com.gomo.app.quest.domain.model.SubjectName;
 import com.gomo.app.quest.domain.repository.AssignQuestRepository;
 import com.gomo.app.quest.domain.repository.RepeatQuestRepository;
 
@@ -39,15 +31,11 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class CreateAssignQuestScheduler {
 
-	private static final String BLANK_SUBJECT_ID = "3bd1b3f7-d7c6-11ef-abb8-a7e09b2a499c";
-	private static final String BLANK_SUBJECT_NAME = "BLANK";
-	private static final String BLANK_SUBJECT_CONTENT = "BLANK";
-
 	private final MemberRepository memberRepository;
 	private final AssignQuestRepository assignQuestRepository;
 	private final RepeatQuestRepository repeatQuestRepository;
 
-	@Scheduled(cron = "0 0 2 * * ?")
+	// @Scheduled(cron = "0 0 2 * * ?")
 	public void createParticipatingQuestOfAllActiveMember() {
 		LocalDateTime startDateTime = LocalDateTime.now();
 		List<Participant> participants = findActiveParticipant(startDateTime.toLocalDate());
@@ -95,31 +83,15 @@ public class CreateAssignQuestScheduler {
 			displayOrder = displayOrder.increase(1);
 		}
 
+		// TODO <jhl221123>: 추후 AI 모델이 생성한 퀘스트 목록을 캐시에서 가져와야 합니다.
+
 		QuestQuota questQuota = participant.getQuestQuota();
-		while(!questQuota.isExceeded(questType, participatingQuests.size())) {
-			AssignQuest assignQuest = createBlankAssignQuest(participantId, questType, displayOrder);
-			participatingQuests.add(assignQuest);
+		while (!questQuota.isExceeded(questType, participatingQuests.size())) {
+			// AssignQuest assignQuest = getQuestFromCache();
+			// participatingQuests.add(assignQuest);
 			displayOrder = displayOrder.increase(1);
 		}
 
 		return participatingQuests;
-	}
-
-	// TODO <jhl221123>: 추후 AI 모델과 연동해 사용자 맞춤 퀘스트 생성하도록 수정이 필요합니다.
-	@NotNull
-	private AssignQuest createBlankAssignQuest(ParticipantId participantId, QuestType questType, DisplayOrder displayOrder) {
-		return AssignQuest.of(
-			AssignQuestId.of(UUIDGenerator.generate()),
-			Quest.of(
-				participantId,
-				SubjectId.of(UUID.fromString(BLANK_SUBJECT_ID)),
-				SubjectName.of(BLANK_SUBJECT_NAME),
-				questType,
-				QuestContent.of(BLANK_SUBJECT_CONTENT)
-			),
-			false,
-			displayOrder,
-			LocalDateTime.now()
-		);
 	}
 }
