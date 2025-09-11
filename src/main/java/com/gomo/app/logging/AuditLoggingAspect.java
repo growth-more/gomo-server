e3738b4af9790c9b1ae6b0b5cc5e46628211edb7
Junhyeok Lee <jhl221123@naver.com>
2025-09-11 16:01:52 +0900

    feat: 로깅 시스템 구축
    - 로깅 환경 설정
    - AOP 기반 로깅 클래스 생성

---
+++ src/main/java/com/gomo/app/logging/AuditLoggingAspect.java
@@ -0,0 +1,40 @@
+package com.gomo.app.logging;
+
+import java.util.Objects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+@Order(2)
+public class AuditLoggingAspect {
+
+	@Around("@annotation(com.gomo.app.logging.AuditLog)")
+	public Object logAuditAction(ProceedingJoinPoint joinPoint) throws Throwable {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		AuditLog auditLog = signature.getMethod().getAnnotation(AuditLog.class);
+		String action = !auditLog.action().isBlank() ?
+			auditLog.action() : signature.getDeclaringType().getSimpleName() + "." + signature.getName();
+
+		try {
+			Object result = joinPoint.proceed();
+			if (Objects.nonNull(result)) {
+				log.info("action={}, status=success, result={}", action, result);
+			} else {
+				log.info("action={}, status=success", action);
+			}
+			return result;
+		} catch (Throwable throwable) {
+			log.error("action={}, status=failed, exception={}", action, throwable.getMessage(), throwable);
+			throw throwable;
+		}
+	}
+}
