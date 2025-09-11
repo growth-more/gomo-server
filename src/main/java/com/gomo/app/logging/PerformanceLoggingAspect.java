db6618fedb569f26c44f124a2442cbbf240163dd
Junhyeok Lee <jhl221123@naver.com>
2025-09-11 16:30:59 +0900

    [GOMO-196] Log4j2 기반 로깅 시스템 구축 (#73)
    * chore: Log4j2 의존성 추가 및 Logback 의존성 제거
    
    * chore: .gitignore 로그 파일 경로 추가
    
    * chore: build.gradle 주석 추가
    
    * test: 회원 생성 http 테스트 추가
    
    * feat: 로깅 시스템 구축
    
    - 로깅 환경 설정
    - AOP 기반 로깅 클래스 생성

---
+++ src/main/java/com/gomo/app/logging/PerformanceLoggingAspect.java
@@ -0,0 +1,36 @@
+package com.gomo.app.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+@Order(1)
+public class PerformanceLoggingAspect {
+
+	@Around("@annotation(com.gomo.app.logging.Timed)")
+	public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+		String className = signature.getDeclaringType().getSimpleName();
+		String methodName = signature.getName();
+
+		StopWatch stopWatch = new StopWatch();
+		try {
+			stopWatch.start();
+			return joinPoint.proceed();
+		} finally {
+			if (stopWatch.isRunning()) {
+				stopWatch.stop();
+			}
+			log.debug("class={}, method={}, executionTime={}ms", className, methodName, stopWatch.getTotalTimeMillis());
+		}
+	}
+}
