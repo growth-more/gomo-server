4e875a41166ef21a77a1b6cb4a49515b6bbb21cd
Junhyeok Lee <jhl221123@naver.com>
2025-09-18 14:06:18 +0900

    refactor: 테스트 sql 경로 변경

--- src/test/java/com/gomo/app/common/DatabaseContainerInitializer.java
+++ src/test/java/com/gomo/app/common/DatabaseContainerInitializer.java
@@ -1,11 +1,14 @@
 package com.gomo.app.common;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
@@ -17,26 +20,36 @@ public class DatabaseContainerInitializer implements ApplicationContextInitializ
 			.withDatabaseName("gomo")
 			.withUsername("gomo")
 			.withPassword("1111")
-			.withInitScript("database/test-schema.sql")
 			.withReuse(true)
 			.waitingFor(Wait.forListeningPort());
 		mysqlContainer.start();
 
+		HikariDataSource ds = new HikariDataSource();
+		ds.setJdbcUrl(mysqlContainer.getJdbcUrl());
+		ds.setUsername(mysqlContainer.getUsername());
+		ds.setPassword(mysqlContainer.getPassword());
+
+		Flyway flyway = Flyway.configure()
+			.dataSource(ds)
+			.locations("classpath:db/migration")
+			.load();
+		flyway.migrate();
+
 		redisContainer = new GenericContainer<>("redis:7.0.8-alpine")
-				.withExposedPorts(6379)
-				.withReuse(true)
-				.waitingFor(Wait.forListeningPort());
+			.withExposedPorts(6379)
+			.withReuse(true)
+			.waitingFor(Wait.forListeningPort());
 		redisContainer.start();
 	}
 
 	@Override
 	public void initialize(ConfigurableApplicationContext context) {
 		TestPropertyValues.of(
-			"spring.datasource.url=" +  mysqlContainer.getJdbcUrl(),
+			"spring.datasource.url=" + mysqlContainer.getJdbcUrl(),
 			"spring.datasource.username=" + mysqlContainer.getUsername(),
 			"spring.datasource.password=" + mysqlContainer.getPassword(),
-				"spring.data.redis.host=" + redisContainer.getHost(),
-				"spring.data.redis.port=" + redisContainer.getMappedPort(6379)
+			"spring.data.redis.host=" + redisContainer.getHost(),
+			"spring.data.redis.port=" + redisContainer.getMappedPort(6379)
 		).applyTo(context.getEnvironment());
 	}
 }
