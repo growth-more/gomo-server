308a5777038fdea046766bb41804afae8982b2f3
Junhyeok Lee <jhl221123@naver.com>
2025-09-21 01:30:54 +0900

    feat: 퀘스트 권한 에러 코드 추가

--- src/main/java/com/gomo/app/quest/exception/code/QuestErrorCode.java
+++ src/main/java/com/gomo/app/quest/exception/code/QuestErrorCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum QuestErrorCode {
 
-	EXCEED_QUOTA(422, "QUE-ROO-001", "Quest quota exceeded");
+	EXCEED_QUOTA(422, "QUE-ROO-001", "Quest quota exceeded"),
+	ACCESS_DENIED(403, "QUE-ROO-002", "Access denied for the quest");
 
 	private final int httpStatus;
 	private final String errorCode;
