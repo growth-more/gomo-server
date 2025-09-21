2f773fa2486cfc7413beb203c444a8c2628470e9
Junhyeok Lee <jhl221123@naver.com>
2025-09-21 21:03:17 +0900

    test: 퀘스트 풀 적재 기능 관련 테스트 추가

---
+++ src/test/java/com/gomo/app/member/application/usecase/ReadActiveMemberUseCaseTest.java
@@ -0,0 +1,38 @@
+package com.gomo.app.member.application.usecase;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.gomo.app.member.application.port.dto.MemberDto;
+import com.gomo.app.member.common.fixture.MemberFixture;
+import com.gomo.app.member.domain.repository.MemberRepository;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("[Application unit]: 활성화 멤버 조회 테스트")
+class ReadActiveMemberUseCaseTest {
+
+	@InjectMocks
+	private ReadActiveMemberUseCase sut;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@DisplayName("활성화 멤버 조회에 성공한다")
+	@Test
+	void read_active_member() {
+		doReturn(List.of(MemberFixture.member(), MemberFixture.member())).when(memberRepository).findByActivateStatusAndLastLoginDateTimeGreaterThanEqual(any(), any());
+		List<MemberDto> actual = sut.findAll(LocalDate.now());
+		assertThat(actual.size()).isEqualTo(2);
+	}
+}
---
+++ src/test/java/com/gomo/app/quest/application/FillQuestPoolUseCaseTest.java
@@ -0,0 +1,72 @@
+package com.gomo.app.quest.application;
+
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.gomo.app.quest.application.port.CreateQuestContentPortOut;
+import com.gomo.app.quest.application.port.ReadActiveParticipantPortOut;
+import com.gomo.app.quest.application.port.ReadSubjectPortOut;
+import com.gomo.app.quest.application.port.dto.ActiveParticipantDto;
+import com.gomo.app.quest.application.port.dto.QuestContentDto;
+import com.gomo.app.quest.application.port.dto.SubjectDto;
+import com.gomo.app.quest.domain.model.QuestType;
+import com.gomo.app.quest.domain.repository.QuestPoolRepository;
+
+@DisplayName("[Application unit]: 퀘스트 풀 생성 테스트")
+@ExtendWith(MockitoExtension.class)
+public class FillQuestPoolUseCaseTest {
+
+	@InjectMocks
+	private FillQuestPoolUseCase sut;
+
+	@Mock
+	private ReadActiveParticipantPortOut readActiveParticipantPortOut;
+
+	@Mock
+	private ReadSubjectPortOut readSubjectPortOut;
+
+	@Mock
+	private CreateQuestContentPortOut createQuestContentPortOut;
+
+	@Mock
+	private QuestPoolRepository questPoolRepository;
+
+	@DisplayName("지정된 타입, 개수만큼 전체 활성화 사용자의 퀘스트 풀을 생성한다.")
+	@Test
+	void fill_quest_pool_for_active_participants() {
+		List<ActiveParticipantDto> activeParticipants = List.of(
+			ActiveParticipantDto.of(UUID.randomUUID()),
+			ActiveParticipantDto.of(UUID.randomUUID())
+		);
+
+		List<SubjectDto> subjects = List.of(
+			SubjectDto.of(UUID.randomUUID(), UUID.randomUUID(), "name1", 10),
+			SubjectDto.of(UUID.randomUUID(), UUID.randomUUID(), "name2", 20),
+			SubjectDto.of(UUID.randomUUID(), UUID.randomUUID(), "name3", 30)
+		);
+
+		List<QuestContentDto> questContents = List.of(
+			QuestContentDto.of(UUID.randomUUID(), UUID.randomUUID(), "name1", "DAILY", "content1"),
+			QuestContentDto.of(UUID.randomUUID(), UUID.randomUUID(), "name2", "DAILY", "content2"),
+			QuestContentDto.of(UUID.randomUUID(), UUID.randomUUID(), "name3", "DAILY", "content3")
+		);
+
+		doReturn(activeParticipants).when(readActiveParticipantPortOut).findAll(any());
+		doReturn(subjects).when(readSubjectPortOut).findAll(any());
+		doReturn(questContents).when(createQuestContentPortOut).create(any());
+
+		sut.fillForAllActiveParticipants(LocalDate.now(), QuestType.DAILY, 3);
+
+		verify(questPoolRepository, times(activeParticipants.size())).saveAll(any());
+	}
+}
