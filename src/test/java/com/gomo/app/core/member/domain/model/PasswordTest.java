80e62d533a74e80669481a910e366426a976e3d4
nurdy-kim <kkj1250@gmail.com>
2025-10-18 21:35:02 +0900

    feat: 비밀번호 검증 규칙 세분화 및 관련 테스트 작성

--- src/test/java/com/gomo/app/core/member/domain/model/PasswordTest.java
+++ src/test/java/com/gomo/app/core/member/domain/model/PasswordTest.java
@@ -21,6 +21,10 @@ public class PasswordTest {
 	private static final String PASSWORD_BLANK = "        ";
 	private static final String TOO_SHORT_PASSWORD = "Test1!";
 	private static final String TOO_LONG_PASSWORD = Stream.generate(() -> PASSWORD).limit(65).collect(Collectors.joining());
+	private static final String NO_LOWERCASE_PASSWORD = "TEST1234!";
+	private static final String NO_UPPERCASE_PASSWORD = "test1234!";
+	private static final String NO_DIGIT_PASSWORD = "TESTTEST!";
+	private static final String NO_SPECIAL_CHAR_PASSWORD = "TEST12345";
 	private static final String FORBIDDEN_PASSWORD = "invalidpassword*";
 
 	@DisplayName("평문 비밀번호를 생성한다.")
@@ -60,6 +64,38 @@ public class PasswordTest {
 		assertThatThrownBy(() -> Password.ofRaw(TOO_LONG_PASSWORD))
 			.isInstanceOf(PasswordConstraintViolationException.class)
 			.hasMessageContaining(PasswordErrorCode.TOO_LONG.getMessage());
+	}
+
+	@DisplayName("소문자가 포함되지 않는 비밀번호는 생성할 수 없다.")
+	@Test
+	void create_password_with_no_lowercase() {
+		assertThatThrownBy(() -> Password.ofRaw(NO_LOWERCASE_PASSWORD))
+			.isInstanceOf(PasswordConstraintViolationException.class)
+			.hasMessageContaining(PasswordErrorCode.NO_LOWERCASE.getMessage());
+	}
+
+	@DisplayName("대문자가 포함되지 않는 비밀번호는 생성할 수 없다.")
+	@Test
+	void create_password_with_no_uppercase() {
+		assertThatThrownBy(() -> Password.ofRaw(NO_UPPERCASE_PASSWORD))
+			.isInstanceOf(PasswordConstraintViolationException.class)
+			.hasMessageContaining(PasswordErrorCode.NO_UPPERCASE.getMessage());
+	}
+
+	@DisplayName("숫자가 포함되지 않는 비밀번호는 생성할 수 없다.")
+	@Test
+	void create_password_with_no_digit() {
+		assertThatThrownBy(() -> Password.ofRaw(NO_DIGIT_PASSWORD))
+			.isInstanceOf(PasswordConstraintViolationException.class)
+			.hasMessageContaining(PasswordErrorCode.NO_DIGIT.getMessage());
+	}
+
+	@DisplayName("특수문자가 포함되지 않는 비밀번호는 생성할 수 없다.")
+	@Test
+	void create_password_with_no_special_char() {
+		assertThatThrownBy(() -> Password.ofRaw(NO_SPECIAL_CHAR_PASSWORD))
+			.isInstanceOf(PasswordConstraintViolationException.class)
+			.hasMessageContaining(PasswordErrorCode.NO_SPECIAL_CHAR.getMessage());
 	}
 
 	@DisplayName("규칙에 위배되는 문자(*)가 들어간 비밀번호는 생성할 수 없다.")
