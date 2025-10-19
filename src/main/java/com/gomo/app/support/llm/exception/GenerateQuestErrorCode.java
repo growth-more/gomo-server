59a4e37693c5c415c589e1569bad9e14f19f11c6
nurdy-kim <kkj1250@gmail.com>
2025-10-19 20:27:35 +0900

    fix: style error 수정

--- src/main/java/com/gomo/app/support/llm/exception/GenerateQuestErrorCode.java
+++ src/main/java/com/gomo/app/support/llm/exception/GenerateQuestErrorCode.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 public enum GenerateQuestErrorCode {
 	GEMINI_API_ERROR(500, "QUE-GEN-001", "An error occurred while call GeminiAPI"),
 	EMPTY_RESPONSE(500, "QUE-GEN-002", "Gemini API response blank"),
-	INVALID_RESPONSE_FORMAT(500, "QUE-GEN-003", "Gemini Response format is invalid")
-	;
+	INVALID_RESPONSE_FORMAT(500, "QUE-GEN-003", "Gemini Response format is invalid");
 
 	private final int httpStatus;
 	private final String errorCode;
